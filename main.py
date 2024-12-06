67473a0873c51bec00456e69424742f47acc1f00
akimakura <akimik2002@gmail.com>
2024-12-06 19:24:54 +0300

    use poetry for version control

---
+++ main.py
@@ -0,0 +1,65 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, EmailStr
+
+import uvicorn
+
+app = FastAPI()
+
+
+class CreateUser(BaseModel):
+    email: EmailStr
+
+@app.get("/")
+def hello_index():
+    return {
+        "message": "Hello World!",
+    }
+
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    name = name.strip().title()
+    return {"message": f"Hello {name}!"}
+
+
+@app.post("/users/")
+def create_user(user: CreateUser):
+    return{
+        "message": "User created successfully!",
+        "email": user.email,
+    }
+
+@app.post("/calc/add/")
+def add(a: int, b: int):
+    return {
+        "a": a,
+        "b": b,
+        "result": a + b,
+    }
+
+@app.get("/items/")
+def list_items():
+    return [
+        "Item1",
+        "Item2",
+        "Item3",
+    ]
+
+
+@app.get("/items/latest/")
+def get_latest_item():
+    return {"item": {"id": "0", "name": "latest"}}
+
+@app.get("/items/{item_id}/")
+def get_item_by_id(item_id: int):
+    return {
+        "item":{
+            "id":item_id,
+        },
+    }
+
+
+
+# Автообновление
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
